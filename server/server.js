de9e33542c830a7c71cfa06da8f0e0fc6aff8ba1
Nicole Torres <nicolejoan9@yahoo.com>
2024-01-08 16:46:29 -0500

    added server.js file as well as dependencies

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const db = require("./config/connection");
+const routes = require("./routes");
+const MongoStore = require("connect-mongo");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 4001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}`);
+  });
+});
