5f194abb2f3b5737dfc323c5dad6784e0a87da3f
Nicole Torres <nicolejoan9@yahoo.com>
2024-01-19 16:02:33 -0500

    added split colors for background

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,8 +1,10 @@
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./home.css";
+import "../pages/home.css";
+import "../../src/App.css";
 import me from "../assets/images/circle-me.jpg";
 
-function home()  {
+function home() {
   return (
     <div>
       <header>
@@ -24,76 +26,102 @@ function home()  {
           </a>
         </div>
       </header>
-      <div className="me-container">
-        {" "}
-        <div className="container-left">
-          <div className="header-name">
-            <h1>Nicole Torres</h1>
-            <div className="container-links">
-              <a href="https://www.linkedin.com/in/nicolet27/">
-                <h6>Linkedin</h6>
-              </a>
-              <h6 className="space">|</h6>
-              <a href="https://github.com/NicoleT27">
-                <h6>Github</h6>
-              </a>
+        <div className="me-container">
+          {" "}
+          <div className="container-left">
+            <div className="header-name">
+              <h1>Nicole Torres</h1>
+              <div className="container-links">
+                <a href="https://www.linkedin.com/in/nicolet27/">
+                  <h6>Linkedin</h6>
+                </a>
+                <h6 className="space">|</h6>
+                <a href="https://github.com/NicoleT27">
+                  <h6>Github</h6>
+                </a>
+              </div>
+              <button className="contact-me">Contact Me</button>
             </div>
-            <button className="contact-me">Contact Me</button>
+          </div>
+          <img className="me" src={me} alt="Image of Nicole" />
+          <div className="header-title">
+            <h3>Introduction</h3>
+            <h6 className="title">Full Stack Web Developer</h6>
+            <p className="paragraph-home">
+              I bring a unique perspective to software engineering, having
+              initially built my career as an Occupational Therapy Assistant.
+              Transitioning into software engineering, I seamlessly blend
+              technical proficiency with a compassionate approach, leveraging my
+              diverse background to drive innovative solutions and contribute
+              meaningfully to projects.
+            </p>
           </div>
         </div>
-        <img className="me" src={me} alt="Image of Nicole" />
-        <div className="header-title">
-          <h3>Introduction</h3>
-          <h6 className="title">Full Stack Web Developer</h6>
-          <p className="paragraph-home">
-            I bring a unique perspective to software engineering, having
-            initially built my career as an Occupational Therapy Assistant.
-            Transitioning into software engineering, I seamlessly blend
-            technical proficiency with a compassionate approach, leveraging my
-            diverse background to drive innovative solutions and contribute
-            meaningfully to projects.
+        <Link to="" className="learn-more">
+          Learn More ↓
+        </Link>
+        <div className="me-container">
+          <div className="header-name"></div>
+          <p>
+            This is where the data goes for the 2nd color. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Et doloremque dignissimos porro
+            tempora! At minima laudantium tenetur minus corrupti aspernatur
+            officiis quaerat maxime aut. Iure reprehenderit alias autem dolores
+            enim?Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Dignissimos ipsam nobis non suscipit asperiores, odio porro esse sed
+            accusantium officiis, exercitationem quisquam modi est consequuntur.
+            Repellendus sunt nemo omnis molestiae!Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Et doloremque dignissimos porro
+            tempora! At minima laudantium tenetur minus corrupti aspernatur
+            officiis quaerat maxime aut. Iure reprehenderit alias autem dolores
+            enim?Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Dignissimos ipsam nobis non suscipit asperiores, odio porro esse sed
+            accusantium officiis, exercitationem quisquam modi est consequuntur.
+            Repellendus sunt nemo omnis molestiae!Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Et doloremque dignissimos porro
+            tempora! At minima laudantium tenetur minus corrupti aspernatur
+            officiis quaerat maxime aut. Iure reprehenderit alias autem dolores
+            enim?Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Dignissimos ipsam nobis non suscipit asperiores, odio porro esse sed
+            accusantium officiis, exercitationem quisquam modi est consequuntur.
+            Repellendus sunt nemo omnis molestiae!
           </p>
         </div>
       </div>
-      <Link to="" className="learn-more">
-        Learn More ↓
-      </Link>
-      <div className="me-container">
-        <div className="header-name"></div>
-      </div>
-    </div>
   );
 }
+
+
 export default home;
 {
-//  <div className="sliders">
-//         <div className="bg-text-container">
-//           <div className="animate-text">
-//             <span className="synonym">Web&nbsp;</span>
-//             <span className="synonym">Developer&nbsp;</span>
-//           </div>
-//           <div className="animate-text left">
-//             <span className="synonym">Software&nbsp;</span>
-//             <span className="synonym">Engineer&nbsp;</span>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="card">
-//         <h1>
-//           {" "}
-//           I am a
-//           <div className="scroller">
-//             <span>
-//               Web Developer
-//               <br />
-//               Software Engineer
-//               <br />
-//               Singer
-//               <br />
-//               Travel Enthusiast
-//             </span>
-//           </div>
-//         </h1>
-//       </div>
-//       <img className="leafBg" src={leafBg} alt="jpg of leaves" /> 
+  //  <div className="sliders">
+  //         <div className="bg-text-container">
+  //           <div className="animate-text">
+  //             <span className="synonym">Web&nbsp;</span>
+  //             <span className="synonym">Developer&nbsp;</span>
+  //           </div>
+  //           <div className="animate-text left">
+  //             <span className="synonym">Software&nbsp;</span>
+  //             <span className="synonym">Engineer&nbsp;</span>
+  //           </div>
+  //         </div>
+  //       </div>
+  //       <div className="card">
+  //         <h1>
+  //           {" "}
+  //           I am a
+  //           <div className="scroller">
+  //             <span>
+  //               Web Developer
+  //               <br />
+  //               Software Engineer
+  //               <br />
+  //               Singer
+  //               <br />
+  //               Travel Enthusiast
+  //             </span>
+  //           </div>
+  //         </h1>
+  //       </div>
+  //       <img className="leafBg" src={leafBg} alt="jpg of leaves" />
 }
