bc7d1950ad072281044ac5da3e7f3e849322334b
Nicole Torres <nicolejoan9@yahoo.com>
2024-01-19 17:08:32 -0500

    installed react scroll, changed learn more to a pointer and removed extra jsx files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
-import About from "./pages/about";
-import Projects from "./pages/projects";
-import Contact from "./pages/contact";
+// import About from "./pages/about";
+// import Projects from "./pages/projects";
+// import Contact from "./pages/contact";
 
 function App() {
 return (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
+      {/* <Route path="about" element={<About />} />
       <Route path="projects" element={<Projects />} />
-      <Route path="contact" element={<Contact />} />
+      <Route path="contact" element={<Contact />} /> */}
     </Routes>
   </BrowserRouter>
 );
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import "../pages/home.css";
+import { Link as RouterLink } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
+import "./home.css";
 import "../../src/App.css";
+import "./contact.css";
+import "./about.css";
 import me from "../assets/images/circle-me.jpg";
 
 function home() {
@@ -13,10 +16,10 @@ function home() {
           <h2 className="dot">.</h2>
         </div>
         <div className="navbar">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/projects">Projects</Link>
-          <Link to="/contact">Contact</Link>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/projects">Projects</RouterLink>
+          <RouterLink to="/contact">Contact</RouterLink>
 
           <a href="https://www.linkedin.com/in/nicolet27/">
             <iconify-icon icon="cib:linkedin"></iconify-icon>
@@ -26,68 +29,79 @@ function home() {
           </a>
         </div>
       </header>
-        <div className="me-container">
-          {" "}
-          <div className="container-left">
-            <div className="header-name">
-              <h1>Nicole Torres</h1>
-              <div className="container-links">
-                <a href="https://www.linkedin.com/in/nicolet27/">
-                  <h6>Linkedin</h6>
-                </a>
-                <h6 className="space">|</h6>
-                <a href="https://github.com/NicoleT27">
-                  <h6>Github</h6>
-                </a>
-              </div>
-              <button className="contact-me">Contact Me</button>
+      <div className="me-container">
+        {" "}
+        <div className="container-left">
+          <div className="header-name">
+            <h1>Nicole Torres</h1>
+            <div className="container-links">
+              <a href="https://www.linkedin.com/in/nicolet27/">
+                <h6>linkedin</h6>
+              </a>
+              <h6 className="space">|</h6>
+              <a href="https://github.com/NicoleT27">
+                <h6>Github</h6>
+              </a>
             </div>
-          </div>
-          <img className="me" src={me} alt="Image of Nicole" />
-          <div className="header-title">
-            <h3>Introduction</h3>
-            <h6 className="title">Full Stack Web Developer</h6>
-            <p className="paragraph-home">
-              I bring a unique perspective to software engineering, having
-              initially built my career as an Occupational Therapy Assistant.
-              Transitioning into software engineering, I seamlessly blend
-              technical proficiency with a compassionate approach, leveraging my
-              diverse background to drive innovative solutions and contribute
-              meaningfully to projects.
-            </p>
+            <button className="contact-me">Contact Me</button>
           </div>
         </div>
-        <Link to="" className="learn-more">
-          Learn More ↓
-        </Link>
-        <div className="me-container">
-          <div className="header-name"></div>
-          <p>
-            This is where the data goes for the 2nd color. Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Et doloremque dignissimos porro
-            tempora! At minima laudantium tenetur minus corrupti aspernatur
-            officiis quaerat maxime aut. Iure reprehenderit alias autem dolores
-            enim?Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Dignissimos ipsam nobis non suscipit asperiores, odio porro esse sed
-            accusantium officiis, exercitationem quisquam modi est consequuntur.
-            Repellendus sunt nemo omnis molestiae!Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Et doloremque dignissimos porro
-            tempora! At minima laudantium tenetur minus corrupti aspernatur
-            officiis quaerat maxime aut. Iure reprehenderit alias autem dolores
-            enim?Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Dignissimos ipsam nobis non suscipit asperiores, odio porro esse sed
-            accusantium officiis, exercitationem quisquam modi est consequuntur.
-            Repellendus sunt nemo omnis molestiae!Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Et doloremque dignissimos porro
-            tempora! At minima laudantium tenetur minus corrupti aspernatur
-            officiis quaerat maxime aut. Iure reprehenderit alias autem dolores
-            enim?Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Dignissimos ipsam nobis non suscipit asperiores, odio porro esse sed
-            accusantium officiis, exercitationem quisquam modi est consequuntur.
-            Repellendus sunt nemo omnis molestiae!
+        <img className="me" src={me} alt="Image of Nicole" />
+        <div className="header-title">
+          <h3>Introduction</h3>
+          <h6 className="title">Full Stack Web Developer</h6>
+          <p className="paragraph-home">
+            I bring a unique perspective to software engineering, having
+            initially built my career as an Occupational Therapy Assistant.
+            Transitioning into software engineering, I seamlessly blend
+            technical proficiency with a compassionate approach, leveraging my
+            diverse background to drive innovative solutions and contribute
+            meaningfully to projects.
           </p>
         </div>
       </div>
+      <ScrollLink
+        to="logo-holder"
+        className="learn-more"
+        smooth={true}
+        duration={500}
+      >
+        Learn More ↓
+      </ScrollLink>
+      <div className="me-container"></div>
+      <div className="header-name"></div>
+
+      <div id="logo-holder" className="logo-holder">
+        <div className="bg">Frameworks/Languages</div>
+        <div className="bar fill0" data-label="Javascript"></div>
+        <div className="bar fill1" data-label="MongoDB"></div>
+        <div className="bar fill2" data-label="CSS"></div>
+        <div className="bar fill3" data-label="React"></div>
+        <div className="bar fill4" data-label="HTML"></div>
+        <div className="bar fill5" data-label="Express.js"></div>
+        <div className="bar fill6" data-label="Node.js"></div>
+        <div className="bar fill7" data-label="SQL"></div>
+      </div>
+      <footer>
+        <div className="letter-image">
+          <div className="animated-mail">
+            <div className="back-fold"></div>
+            <div className="letter">
+              <div className="letter-border"></div>
+              <div className="letter-title"></div>
+              <div className="letter-context"></div>
+              <div className="letter-stamp">
+                <div className="letter-stamp-inner"></div>
+              </div>
+            </div>
+            <div className="top-fold"></div>
+            <div className="body"></div>
+            <div className="left-fold"></div>
+          </div>
+          <div className="shadow"></div>
+        </div>
+      </footer>
+    </div>
   );
 }
 
