1aa6d76d1b1286ed0f238175e1a78459728fa872
Nicole Torres <nicolejoan9@yahoo.com>
2024-01-29 21:16:09 -0500

    removed unused dependencies

--- README.md
+++ README.md
@@ -30,7 +30,6 @@ This webpage was created to provide information on Nicole Torres and her experie
 ![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB)
 ![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)
 ![MongoDB](https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white)
-![Threejs](https://img.shields.io/badge/threejs-black?style=for-the-badge&logo=three.js&logoColor=white)
 ![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
 
 ## Usage
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,40 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-// const nodemailer = require("nodemailer");
-
-// //API routes
-// router.use("/api", apiRouter);
-
-// router.post("/", (req, res) => {
-//   const { name, email, phone, subject, message } = req.body;
-
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.EMAIL,
-//       pass: process.env.PASSWORD,
-//     },
-//   });
-
-//   const mailOptions = {
-//     from: email,
-//     to: "nicolejoan9@yahoo.com",
-//     subject: subject,
-//     text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
-//   };
-
-//   transporter.sendMail(mailOptions, function (error, data) {
-//     if (error) {
-//       console.log(error);
-//       res.status(500).json({ success: false, message: "An error has occurred" });
-//       // res.send("An error has occurred");
-//     } else {
-//       // res.send("Message was sent successfully");
-//       res.json({ success: true, message: "Message was sent successfully" });
-//     }
-//   });
-// });
 
 
 //connects server to our client folder
